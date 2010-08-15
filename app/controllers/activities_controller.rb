c7f03b741eacdfeca7e1172943bb03ebb9184e2b
John Britton <public@johndbritton.com>
2010-08-15 11:05:27 -0400

    added user filtering to activites

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,9 +2,16 @@ class ActivitiesController < ApplicationController
   # GET /activities
   # GET /activities.xml
   def index
-    @activities = Pitch.all + Vote.all + Membership.all
+    unless params[:user_id]
+      @activities = Pitch.all + Vote.all + Membership.all
+    else
+      pitches = Pitch.find(:all, :conditions => ["user_id = ?", params[:user_id]])
+      votes = Vote.find(:all, :conditions => ["user_id = ?", params[:user_id]])
+      memberships = Membership.find(:all, :conditions => ["user_id = ?", params[:user_id]])
+      @activities = pitches + votes + memberships
+    end
     @activities.sort! { |a,b| b.created_at <=> a.created_at }
-  
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @activities }
